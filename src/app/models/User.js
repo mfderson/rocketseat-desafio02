f1f6be6c8a64ef16e45ab5ae197af39fbbef9079
Anderson <mfderson@gmail.com>
2019-08-31 09:11:50 -0300

    Criando model de usuário

---
+++ src/app/models/User.js
@@ -0,0 +1,20 @@
+import { Model, Sequelize } from 'sequelize';
+
+class User extends Model {
+  // Este método é chamado automaticamente pelo sequelize
+  static init(sequelize) {
+    // Contém todas as colunas excluindo PK, FK, created_at e updated_at
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+export default User;
