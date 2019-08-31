26ed0b4354b7c7df93d7f568b0e754b3d771e0d7
Anderson <mfderson@gmail.com>
2019-08-31 10:40:19 -0300

    Adicionando middleware auth e pegando o token da requisição

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,16 @@ import User from './app/models/User';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+// Este middleware será aplicado apenas para as rotas que estão abaixo
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
