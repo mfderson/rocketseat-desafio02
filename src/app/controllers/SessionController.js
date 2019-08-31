0e796aa65be87fe07ffffed814942865be71ae15
Anderson <mfderson@gmail.com>
2019-08-31 10:24:48 -0300

    Adicionando sessionController

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,7 +1,33 @@
+import jwt from 'jsonwebtoken';
 import User from '../models/User';
 
 class SessionController {
-  async store(req, res) {}
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, 'e52d23a3d2be852e10e0a76b97e1ecf1', {
+        expiresIn: '7d',
+      }),
+    });
+  }
 }
 
 export default new SessionController();
