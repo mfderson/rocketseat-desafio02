ca5ff2141834ee5a81d0785834cddcc438ad1ce6
Anderson <mfderson@gmail.com>
2019-09-11 18:58:30 -0300

    feature: Só pode cadastrar um meetup se a data não for passada

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,17 +1,25 @@
 import * as Yup from 'yup';
+import { parseISO, isBefore } from 'date-fns';
 import Meetup from '../models/Meetup';
 
 class MeetupController {
   async store(req, res) {
     const schema = Yup.object().shape({
+      file_id: Yup.number().required(),
       title: Yup.string().required(),
       description: Yup.string().required(),
       location: Yup.string().required(),
       date: Yup.date().required(),
     });
 
+    const { date } = req.body;
+
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    if (isBefore(parseISO(date), new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
     }
 
     const user_id = req.userId;
