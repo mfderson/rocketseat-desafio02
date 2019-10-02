4a87ff961b826f7c7e7d730d8bc247f24bf9120c
Anderson <mfderson@gmail.com>
2019-10-02 19:56:50 -0300

    chore: adicionando validação que o usuário não pode se inscrever no próprio meetup

--- src/app/controllers/SubscriptionController.js
+++ src/app/controllers/SubscriptionController.js
@@ -1,5 +1,16 @@
+import Meetup from '../models/Meetup';
+
 class SubscriptionController {
   async store(req, res) {
+    console.log('entrou no SubscriptionController.store');
+    const meetup = await Meetup.findByPk(req.params.meetupId);
+
+    if (meetup.user_id === req.userId) {
+      return res
+        .status(400)
+        .json({ error: "Can't subscribe to you own meetups" });
+    }
+
     return res.json();
   }
 }
