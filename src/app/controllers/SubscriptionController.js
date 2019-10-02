f09e657167aa6c1d25c57f1f88e929e38b9981ea
Anderson <mfderson@gmail.com>
2019-10-02 20:00:30 -0300

    chore: validação, não pode se inscrever em meetups passados

--- src/app/controllers/SubscriptionController.js
+++ src/app/controllers/SubscriptionController.js
@@ -2,13 +2,16 @@ import Meetup from '../models/Meetup';
 
 class SubscriptionController {
   async store(req, res) {
-    console.log('entrou no SubscriptionController.store');
     const meetup = await Meetup.findByPk(req.params.meetupId);
 
     if (meetup.user_id === req.userId) {
       return res
         .status(400)
         .json({ error: "Can't subscribe to you own meetups" });
+    }
+
+    if (meetup.past) {
+      return res.status(400).json({ error: "Can't subscribe to past meetups" });
     }
 
     return res.json();
